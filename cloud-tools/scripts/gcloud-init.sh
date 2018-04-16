de298348d131c7543e0bb409f2b105ec0ef76555
Ameer Abbas <ameer00@gmail.com>
2018-04-16 16:02:39 -0700

    Update gcloud-init.sh

--- cloud-tools/scripts/gcloud-init.sh
+++ cloud-tools/scripts/gcloud-init.sh
@@ -4,7 +4,7 @@ if [[ -z "${DEVSHELL_PROJECT_ID}" ]]; then
     echo ">>>>> Initializing gcloud with your credentials (assumed to be in $HOME)"
     echo -e "\n>>>>> Enter the name of your credential key: "
     read credentialkeyfile
-    gcloud auth activate-service-account --key-file=$HOME/multicloud/$credentialkeyfile
+    gcloud auth activate-service-account --key-file=$HOME/$credentialkeyfile
 fi
 
 echo "You'll want to now run 'gcloud config set project <project ID>'"
