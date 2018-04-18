a4b86e8e7e3e397570d8583b1aa1518a8f689d9e
ameer00 <ameer00@gmail.com>
2018-04-18 09:27:39 -0700

    add vars.sh again

--- cloud-tools/scripts/vars.sh
+++ cloud-tools/scripts/vars.sh
@@ -2,7 +2,6 @@
 # Feel free to add additional variables underneath the GCP and AWS sections below
 
 # Enter values for GCP
-export PROJECT=ameer-mc-2
 export GCP_ZONE=us-central1-a
 export GKE_CLUSTER=gke-cluster
 export GLB_CLUSTER=glb-cluster
@@ -11,9 +10,10 @@ export GLB_CLUSTER=glb-cluster
 export AWS_REGION=us-east-2
 export AWS_ZONE=us-east-2a
 export DOMAIN=ameerlabs.com
-export AWS_CLUSTER=k8s
+export AWS_CLUSTER=$AWS_ZONE
 
 # Do not enter anything below
+export PROJECT=$(gcloud info --format='value(config.project)')
 export GKE_CONTEXT="gke_"$PROJECT"_"$GCP_ZONE"_"$GKE_CLUSTER
 export GLB_CONTEXT="gke_"$PROJECT"_"$GCP_ZONE"_"$GLB_CLUSTER
 export AWS_CONTEXT=$AWS_CLUSTER".aws."$DOMAIN
